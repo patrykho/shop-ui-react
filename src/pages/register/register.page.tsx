d46c1da6b24d46e836d49e09d86855b78de357aa
Patrick Holody <patryk.holody@gmail.com>
2020-05-26 00:03:23 +0200

    add register page

--- src/pages/register/register.page.tsx
+++ src/pages/register/register.page.tsx
@@ -1,9 +1,167 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Avatar,
+  Container,
+  CssBaseline,
+  Typography,
+  TextField,
+  Button,
+  Grid,
+  Link,
+  makeStyles,
+} from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { Redirect } from 'react-router-dom';
 
-interface RegisterProps {}
+import AuthApi from '../../api/auth.api';
+import useFormState from '../../hooks/use-form-state';
+import ErrorMessages from '../../components/error-messages';
+import { removeToken } from '../../services/access-token.service';
 
-const Register = (props: RegisterProps) => {
-  return <div>Register</div>;
+import { RegisterDataI } from '../../interfaces/register-data-interface';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const initialState: RegisterDataI = {
+  login: '',
+  firstName: '',
+  lastName: '',
+  password: '',
+};
+
+const Register = () => {
+  const [formState, setFormState] = useFormState(initialState);
+  const [error, setError] = useState<undefined | string | string[]>();
+  const [isRegister, setIsRegister] = useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    event.preventDefault();
+    setFormState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    try {
+      await AuthApi.signUp(formState);
+      removeToken();
+      setIsRegister(true);
+    } catch (error) {
+      const errorResponse = error.response.data.message;
+      setError(errorResponse);
+    }
+  };
+
+  const classes = useStyles();
+
+  if (isRegister) {
+    return (
+      <Redirect
+        to={{
+          pathname: '/login',
+          state: { from: '/register' },
+        }}
+      />
+    );
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Register
+        </Typography>
+        {error && <ErrorMessages errors={error} />}
+        <form className={classes.form}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="firstName"
+                variant="outlined"
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type="email"
+                variant="outlined"
+                fullWidth
+                id="login"
+                label="Email Address for Login"
+                name="login"
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={handleSubmit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Already have an account? go to Login Page ->
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
 };
 
 export default Register;
