373fe6e73ce938ff79c350c7915ec684d6e88ec2
Patrick Holody <patryk.holody@gmail.com>
2020-05-26 09:25:28 +0200

    add navBar and update Products component

---
+++ src/components/nav-bar/nav-bar.component.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  makeStyles,
+  Theme,
+  createStyles,
+} from '@material-ui/core';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import { Link } from 'react-router-dom';
+
+import './nav-bar.component.scss';
+
+import { removeToken } from '../../services/access-token.service';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
+      },
+    },
+
+    inputRoot: {
+      color: 'inherit',
+    },
+    sectionDesktop: {
+      display: 'none',
+      [theme.breakpoints.up('md')]: {
+        display: 'flex',
+      },
+    },
+  })
+);
+
+interface NavBarProps {
+  isLogin: boolean;
+  onSetIsLogin: Function;
+}
+
+const NavBar = (props: NavBarProps) => {
+  const classes = useStyles();
+  const { isLogin, onSetIsLogin } = props;
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography className={classes.title} variant="h6">
+            Shop Management
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            {!isLogin && (
+              <>
+                <Button color="inherit">
+                  <Link to="/login">Login</Link>
+                </Button>
+                <Button color="inherit">
+                  <Link to="/register">Register</Link>
+                </Button>
+              </>
+            )}
+            {isLogin && (
+              <>
+                <Button
+                  onClick={() => {
+                    removeToken();
+                    onSetIsLogin(false);
+                  }}
+                  color="inherit"
+                >
+                  Logout
+                </Button>
+                <IconButton
+                  edge="end"
+                  aria-label="account of current user"
+                  aria-haspopup="true"
+                  onClick={() => {
+                    console.log('go to user Profile');
+                  }}
+                  color="inherit"
+                >
+                  <Link to="/profile">
+                    <AccountCircle />
+                  </Link>
+                </IconButton>
+              </>
+            )}
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/pages/products/products.page.tsx
+++ src/pages/products/products.page.tsx
@@ -1,9 +1,156 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  Typography,
+  Button,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Avatar,
+  makeStyles,
+  createStyles,
+  Theme,
+} from '@material-ui/core';
+import CreateIcon from '@material-ui/icons/Create';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { getToken } from '../../services/access-token.service';
+import { isTokenExpired } from '../../services/jwt-service';
+import NavBar from '../../components/nav-bar/nav-bar.component';
 
 interface ProductsProps {}
 
+const PRODUCTS = [
+  {
+    id: 3,
+    title: 'APPLE MacBook Pro 16 i5',
+    imageUrl:
+      'https://www.mediaexpert.pl/media/cache/gallery/product/2/295/116/565/1vjqtsjy/images/20/2097809/APPLE-MacBook-Pro-16-front.jpg',
+    description:
+      'Oto najpotężniejszy notebook od Apple. Stworzony dla twórców. Ma niesamowity wyświetlacz Retina o przekątnej 16 cali, superszybkie procesory, grafikę nowej generacji, najpojemniejszą baterię w historii MacBooka Pro, nową klawiaturę Magic Keyboard i olbrzymią pamięć masową. To najdoskonalszy sprzęt dla najbardziej wymagających profesjonalistów.',
+    price: 11224.33,
+  },
+  {
+    id: 1,
+    title: 'APPLE MacBook Pro 16 i5',
+    imageUrl:
+      'https://www.mediaexpert.pl/media/cache/gallery/product/2/295/116/565/1vjqtsjy/images/20/2097809/APPLE-MacBook-Pro-16-front.jpg',
+    description:
+      'Oto najpotężniejszy notebook od Apple. Stworzony dla twórców. Ma niesamowity wyświetlacz Retina o przekątnej 16 cali, superszybkie procesory, grafikę nowej generacji, najpojemniejszą baterię w historii MacBooka Pro, nową klawiaturę Magic Keyboard i olbrzymią pamięć masową. To najdoskonalszy sprzęt dla najbardziej wymagających profesjonalistów.',
+    price: 1111.44,
+  },
+  {
+    id: 4,
+    title: 'APPLE MacBook Pro 16 i5',
+    imageUrl:
+      'https://ww.mediaexpert.pl/media/cache/gallery/product/2/295/116/565/1vjqtsjy/images/20/2097809/APPLE-MacBook-Pro-16-front.jpg',
+    description:
+      'Oto najpotężniejszy notebook od Apple. Stworzony dla twórców. Ma niesamowity wyświetlacz Retina o przekątnej 16 cali, superszybkie procesory, grafikę nowej generacji, najpojemniejszą baterię w historii MacBooka Pro, nową klawiaturę Magic Keyboard i olbrzymią pamięć masową. To najdoskonalszy sprzęt dla najbardziej wymagających profesjonalistów.',
+    price: 11224.33,
+  },
+];
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    small: {
+      width: theme.spacing(3),
+      height: theme.spacing(3),
+    },
+    large: {
+      width: theme.spacing(7),
+      height: theme.spacing(7),
+    },
+  })
+);
+
 const Products = (props: ProductsProps) => {
-  return <div>Products</div>;
+  const classes = useStyles();
+  const [products, setProducts] = useState(PRODUCTS);
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    const token: string | null = getToken();
+    if (token) {
+      const validToken: boolean = isTokenExpired(token);
+      if (validToken) {
+        setIsLogin(true);
+      }
+    }
+  }, []);
+
+  return (
+    <div>
+      <NavBar isLogin={isLogin} onSetIsLogin={setIsLogin} />
+      <Typography variant="h4">Products list</Typography>
+      <Button
+        disabled={!isLogin}
+        variant="outlined"
+        color="primary"
+        onClick={() => console.log('add Product')}
+      >
+        Add Product
+      </Button>
+
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>#</TableCell>
+            <TableCell>Image</TableCell>
+            <TableCell>Title</TableCell>
+            <TableCell>Price</TableCell>
+            <TableCell>Edit</TableCell>
+            <TableCell>Delete</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {products.map((row, index) => (
+            <TableRow key={row.id}>
+              <TableCell component="th" scope="row">
+                {index + 1}
+              </TableCell>
+              <TableCell>
+                <Avatar
+                  alt="Remy Sharp"
+                  src={row.imageUrl}
+                  className={classes.large}
+                />
+              </TableCell>
+              <TableCell>{row.title}</TableCell>
+
+              <TableCell>{row.price}</TableCell>
+
+              <TableCell>
+                <Button
+                  disabled={!isLogin}
+                  variant="contained"
+                  color="primary"
+                  onClick={() => console.log('edit Product', row.id)}
+                >
+                  <CreateIcon />
+                </Button>
+              </TableCell>
+              <TableCell>
+                <Button
+                  disabled={!isLogin}
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => console.log('delete Product', row.id)}
+                >
+                  <DeleteIcon />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
 
 export default Products;
